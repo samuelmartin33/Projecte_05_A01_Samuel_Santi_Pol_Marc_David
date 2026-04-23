3d3fd8e667ad78d9192c5687963249208d9e4f53
DavidHonoratoFernandez <463507.joan23@fje.edu>
2026-04-23 16:25:54 +0200

    Solución Error Build

--- vite.config.js
+++ vite.config.js
@@ -7,13 +7,11 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/style.css',
-                'resources/css/admin.css',
                 'resources/css/admin-eventos.css',
                 'resources/js/app.js',
                 'resources/js/login.js',
                 'resources/js/register.js',
                 'resources/js/index.js',
-                'resources/js/admin.js',
                 'resources/js/admin-eventos.js',
                 'resources/js/perfil.js',
             ],
