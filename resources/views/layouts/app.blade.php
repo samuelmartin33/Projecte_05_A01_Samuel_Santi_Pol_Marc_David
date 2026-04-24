49f7f2fe485d30742d06d3339eb98df768ede39c
PolCasadesus <100007861.joan23@fje.edu>
2026-04-24 18:05:37 +0200

    Entradas con QR

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -117,6 +117,15 @@
                                 Mi perfil
                             </a>
 
+                            {{-- Mis entradas --}}
+                            <a href="{{ route('entradas.mis-entradas') }}" class="nav-dropdown-item">
+                                <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                          d="M15 5v2m0 4v2m0 4v2M5 5a2 2 0 00-2 2v3a2 2 0 110 4v3a2 2 0 002 2h14a2 2 0 002-2v-3a2 2 0 110-4V7a2 2 0 00-2-2H5z"/>
+                                </svg>
+                                Mis entradas
+                            </a>
+
                             {{-- Amigos --}}
                             <a href="{{ route('perfil') }}#amigos" class="nav-dropdown-item">
                                 <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
